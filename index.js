b898d073ef346a3e5c89bab9ca2ebc72f4243114
Ranjan <m2rm.bng@gmail.com>
2025-08-04 20:44:54 +0530

    env updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 const app = express();
 
